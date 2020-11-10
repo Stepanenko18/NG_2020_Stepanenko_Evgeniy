abb657185a15904e04c74919892dc47bafda2c7e
Stepanenko18 <thejohnny26@gmail.com>
2020-11-10 21:30:45 +0200

    3 task complete

---
+++ Lesson 1/Task1.3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "Skolko oreshkov nuzhno belochke?: ";
+    cin >> a;
+    cout << "Skolko oreshkov v odnoy shishke?: ";
+    cin >> b;
+    cout << "Skolko shishek bylo sobrano?: ";
+    cin >> c;
+    if (c*b >=a)
+        {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" <<endl;
+    }
+}
+
