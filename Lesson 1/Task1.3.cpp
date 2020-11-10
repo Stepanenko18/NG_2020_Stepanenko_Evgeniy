7b2148eb5977dfc6677b920f173a9fad4e20e34b
Stepanenko18 <thejohnny26@gmail.com>
2020-11-10 21:59:49 +0200

    3 task change

--- Lesson 1/Task1.3.cpp
+++ Lesson 1/Task1.3.cpp
@@ -5,17 +5,20 @@ using namespace std;
 int main()
 {
     int a,b,c;
+
     cout << "Skolko oreshkov nuzhno belochke?: ";
     cin >> a;
     cout << "Skolko oreshkov v odnoy shishke?: ";
     cin >> b;
     cout << "Skolko shishek bylo sobrano?: ";
     cin >> c;
-    if (c*b >=a)
-        {
+
+    if (c * b >= a)
+    {
         cout << "YES" << endl;
     }
-    else {
+    else
+    {
         cout << "NO" <<endl;
     }
 }
