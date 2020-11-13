c79bae6ab1b23683bdcd22365f841458bd88f075
Stepanenko18 <thejohnny26@gmail.com>
2020-11-13 22:58:43 +0200

    complete

---
+++ Lesson 1/Task1.7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 0, a;
+    cout << "Enter number: ";
+    cin >> a;
+    for(i = 0; i < a; i++)
+    {
+        cout << i;
+        cout << ",";
+    }
+        cout << i;
+}
